dd58841f8dc0cd4a0464643538f70844e55c5247
Nguyen Dang <nguyendang6698@gmail.com>
2021-08-11 11:02:49 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful save
+      flash[:success] = "Chao mung ban den voi trang web an hai cua toi!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "",
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post signup_path, params: { user: { name: "nguyendang",
+                                          email: "nguyendang@mail.com",
+                                          password: "dang123",
+                                          password_confirmation: "dang123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash[:success].blank?
+    assert_select 'div[class=?]','alert alert-success'
+  end
+
+
+end
