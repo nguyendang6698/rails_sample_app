07e13d5472d50e6e9d5258c9497b4c1903e463d6
Nguyen Dang <nguyendang6698@gmail.com>
2021-08-16 17:37:33 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save { email.downcase! }
 	before_create :create_activation_digest
 	validates :name, presence: true, length: {maximum: 50}
@@ -33,20 +33,13 @@ class User < ApplicationRecord
 	# Returns true if the given token matches the digest.
 	def authenticated?(attribute, token)
 		digest = self.send("#{attribute}_digest")
-		if digest
-			BCrypt::Password.new(digest).is_password?(token)
-		end
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	# Forgets a user.
 	def forget
 	  update_attribute(:remember_digest, nil)
-	end
-
-	# Creates and assigns the activation token and digest.
-	def create_activation_digest
-		self.activation_token = User.new_token
-		self.activation_digest = User.digest(activation_token)
 	end
 
 	# Activates an account
@@ -59,4 +52,29 @@ class User < ApplicationRecord
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
 	end
+
+	# Sets the password reset attributes.
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# Sends password reset email.
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+
+	# Returns true if password reset has expired
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+
+	private
+
+		# Creates and assigns the activation token and digest.
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
